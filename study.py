5525cdeeea51aecb03787f26be4f8feeb29f9479
Kara Weisman <kgweisman@gmail.com>
2014-01-19 17:46:29 -0800

    Sketched in trial templating system.

--- study.py
+++ study.py
@@ -1,13 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+import yaml
+
 study = Flask(__name__)
 
-@study.route('/intro/<slug>')
-def intro(slug):
-	return 'intro: ' + slug
 
-@study.route('/<sequence>/<trial>')
-def trial(sequence, trial):
-	return 'sequence: ' + sequence + '| trial: ' + trial
+# HELPERS
+
+def load_items():
+	return yaml.load(open('config/items.yaml', 'r'))
+
+def load_sequences():
+	return yaml.load(open('config/sequences.yaml', 'r'))
+
+def get_item(item):
+	return load_items()[item]
+
+def get_sequence(sid):
+	return load_sequences()['seq' + sid]
+
+def get_trial_pair(sid, tid):
+
+    trial = get_sequence(sid)[int(tid)-1]
+    b = get_item(trial[0])
+    q = get_item(trial[1])
+    
+    return { 'base': b, 'question': q }
+
+
+
+# ACTIONS
+
+@study.route('/sequence/<sid>/trial/<tid>')
+def trial(sid, tid):	
+	pair = get_trial_pair(sid, tid)
+	return render_template('trial.html', pair = pair)
+
 
 if __name__ == '__main__':
 	study.run(debug = True)
